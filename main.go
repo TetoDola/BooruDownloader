7bf05c4274d20b5795f66b8a14f5df8b34c02d1e
TetoDola <sudaravan10@icloud.com>
2025-04-18 16:29:20 +0200

    Parsing Html Done

--- main.go
+++ main.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func main() {
 	url := "https://safebooru.org/index.php?page=post&s=view&id=5708103"
-	req, err := http.Get(url)
+	response, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
+
 		return
 	} else {
-		fmt.Println(req)
+		_, _ = fmt.Println(response) // the underscore is a way to say ik it returns something and something but i will ignore it
+		defer response.Body.Close()
+		html, err := io.ReadAll(response.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		_, _ = fmt.Println(string(html))
 	}
 }
+
+// TODO: Now that we fetched the html, we need to identify main booru image and download it somehow
