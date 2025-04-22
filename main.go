13a5a3826f6f9b7295d412dd00e34b37fd13af78
TetoDola <sudaravan10@icloud.com>
2025-04-22 23:59:22 +0200

    error handling fix

--- main.go
+++ main.go
@@ -11,55 +11,38 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+func check_error(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 func main() {
 	url := "https://safebooru.org/index.php?page=post&s=view&id=5708103"
 	response, err := http.Get(url)
-	if err != nil {
-		fmt.Println(err)
-
-		return
-	} else {
-		_, _ = fmt.Println(response) // the underscore is a way to say ik it returns something and something but i will ignore it
-		defer response.Body.Close()
-		html, err := io.ReadAll(response.Body)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		html_str := string(html)
-		doc, err := goquery.NewDocumentFromReader(strings.NewReader(html_str))
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		image_url := doc.Find("meta[property='og:image']").AttrOr("content", "")
-		fmt.Println(image_url)
-		download, err := http.Get(image_url)
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = download.Body.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-		file, err := os.Create("image.jpg")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		DownloadedImage, err := io.Copy(file, download.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = file.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(download, DownloadedImage)
-		return
-	}
-	// TODO: Properly handle error
-	// TODO: Do it in a loop between image ids
-	// ToDO: Create Folder before Saving
+	check_error(err)
+	_, _ = fmt.Println(response) // the underscore is a way to say ik it returns something and something but i will ignore it
+	defer response.Body.Close()
+	html, err := io.ReadAll(response.Body)
+	check_error(err)
+	html_str := string(html)
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html_str))
+	check_error(err)
+	image_url := doc.Find("meta[property='og:image']").AttrOr("content", "")
+	fmt.Println(image_url)
+	download, err := http.Get(image_url)
+	check_error(err)
+	file, err := os.Create("image.jpg")
+	check_error(err)
+	DownloadedImage, err := io.Copy(file, download.Body)
+	check_error(err)
+	err = file.Close()
+	check_error(err)
+	fmt.Println(download, DownloadedImage)
+	err = download.Body.Close()
+	check_error(err)
+	return
 }
+
+// TODO: Properly handle error
+// TODO: Do it in a loop between image ids
+// ToDO: Create Folder before Saving
