7ce703422b5a085866147e9f49961d80b8549c4a
TetoDola <sudaravan10@icloud.com>
2025-04-23 11:04:48 +0200

    better readability

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"strings"
-
-	"github.com/PuerkitoBio/goquery"
 )
 
 func check_error(err error) {
@@ -17,25 +16,8 @@ func check_error(err error) {
 	}
 }
 
-func main() {
-	// Fetch HTML
-	url := "https://safebooru.org/index.php?page=post&s=view&id=5708103"
-	response, err := http.Get(url)
-	check_error(err)
-	_, _ = fmt.Println(response) // the underscore is a way to say ik it returns something and something but i will ignore it
-	defer response.Body.Close()
-
-	// Parse HTML
-	html, err := io.ReadAll(response.Body)
-	check_error(err)
-	html_str := string(html)
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html_str))
-	check_error(err)
-	image_url := doc.Find("meta[property='og:image']").AttrOr("content", "")
-	fmt.Println(image_url)
-
-	// Fetch Img + Download
-	download, err := http.Get(image_url)
+func download_image(url string) {
+	download, err := http.Get(url)
 	check_error(err)
 	file, err := os.Create("image.jpg")
 	check_error(err)
@@ -46,7 +28,26 @@ func main() {
 	fmt.Println(download, DownloadedImage)
 	err = download.Body.Close()
 	check_error(err)
-	return
+}
+
+func parse_html(html string) {
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	check_error(err)
+	image_url := doc.Find("meta[property='og:image']").AttrOr("content", "")
+	fmt.Println(image_url)
+	download_image(image_url)
+}
+func main() {
+	// Fetch HTML
+	url := "https://safebooru.org/index.php?page=post&s=view&id=5708103"
+	response, err := http.Get(url)
+	check_error(err)
+	_, _ = fmt.Println(response) // the underscore is a way to say ik it returns something and something but i will ignore it
+	html, err := io.ReadAll(response.Body)
+	check_error(err)
+	parse_html(string(html))
+	defer response.Body.Close()
+
 }
 
 // TODO: Properly handle error
